7e0f48141af19bc1e4f585df914182e506b7cdfa
Edy Rodriguez <Edy Rodriguez@LAPTOP-QFB1AMLV>
2023-04-07 18:25:50 -0600

    Changed printed message

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -4,11 +4,6 @@ public class Day2Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Day 2 Git");
-
-	}
-
-	public void divid(int a, int b) {
-		System.out.println(a / b);
+		System.out.println("Pushing code");
 	}
 }
