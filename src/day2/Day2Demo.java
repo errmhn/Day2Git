f72639c760a53228fe795468fdf11a72ab7a7ce3
Edy Rodriguez <Edy Rodriguez@LAPTOP-QFB1AMLV>
2023-04-07 18:37:43 -0600

    Added stash message

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -5,5 +5,6 @@ public class Day2Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pushing code");
+		System.out.println("Practicing Stash and Push");
 	}
 }
