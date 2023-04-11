663f4ac06eb195678af69d063749fd89a330999b
errmhn <129729132+errmhn@users.noreply.github.com>
2023-04-10 19:03:47 -0600

    Added new message line

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -7,6 +7,6 @@ public class Day2Demo {
 		System.out.println("Pushing code");
 		System.out.println("Practicing Stash and Push");
 		System.out.println("Pull test");
-		
+		System.out.println("Adding new pull");
 	}
 }
