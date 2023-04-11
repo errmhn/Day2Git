24a3b8d6c6a2b5f1f9858e9d28129bf56ca0481f
Edy Rodriguez <Edy Rodriguez@LAPTOP-QFB1AMLV>
2023-04-10 19:08:11 -0600

    Added variable

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -3,10 +3,12 @@ package day2;
 public class Day2Demo {
 
 	public static void main(String[] args) {
+		int a = 0;
+		
 		// TODO Auto-generated method stub
 		System.out.println("Pushing code");
 		System.out.println("Practicing Stash and Push");
 		System.out.println("Pull test");
 		System.out.println("Adding new pull");
-	}
+		}
 }
